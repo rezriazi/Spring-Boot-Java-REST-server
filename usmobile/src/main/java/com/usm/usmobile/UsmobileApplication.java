e4f6b68c1297c566cfee00b57f54a6ea9d0dd83e
rezriazi <60489397+rezriazi@users.noreply.github.com>
2024-07-04 18:37:24 +0100

    initialize packages

---
+++ usmobile/src/main/java/com/usm/usmobile/UsmobileApplication.java
@@ -0,0 +1,13 @@
+package com.usm.usmobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsmobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsmobileApplication.class, args);
+	}
+
+}
---
+++ usmobile/src/test/java/com/usm/usmobile/UsmobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usm.usmobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsmobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
